30f27f2ebd44b879cac3ccec35c91a4c261f08fa
Deepak2909 <deepak19csu075@ncuindia.edu>
2021-05-02 16:15:58 +0530

    Initial Commit

---
+++ TableView/TableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewApp.swift
+//  TableView
+//
+//  Created by Deepak on 02/05/21.
+//
+
+import SwiftUI
+
+@main
+struct TableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
